b6ff9992da66f104f6dc20107455788493453e92
Lordran <ddddnxzy@gmail.com>
2020-06-13 07:48:17 +0800

    Update cpyenv.sh

--- cpyenv.sh
+++ cpyenv.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl git libmysqlclient-dev
 
 
 v=$1
