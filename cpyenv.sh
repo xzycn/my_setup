7be383e3f620eb294099e8fae1d43a196e57c1ac
Lordran <ddddnxzy@gmail.com>
2020-06-13 06:56:50 +0800

    Update cpyenv.sh

--- cpyenv.sh
+++ cpyenv.sh
@@ -1,4 +1,6 @@
-sudo apt-get install -y zlib1g-dev
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 
 
 v=$1
